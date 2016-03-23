6150fbab2f94e0e758171658e63a4b7724940ff8
Douglas Martins <douglas.pac@gmail.com>
2016-03-23 12:18:20 -0300

    BugEx10 estava pedindo 11 numeros. Corrigido

--- app/src/main/java/br/com/douglaspac/androidnivelamento/Ex10.java
+++ app/src/main/java/br/com/douglaspac/androidnivelamento/Ex10.java
@@ -14,10 +14,10 @@ public class Ex10 extends AppCompatActivity {
     Button criarVetor;
     EditText v1;
     ArrayList<Integer> vetor;
-    int soma=0;
     int cont=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex10);
         v1 = (EditText)findViewById(R.id.vetor1);
@@ -30,14 +30,20 @@ public class Ex10 extends AppCompatActivity {
             public void onClick(View v) {
                 if(cont!=10){
                     vetor.add(Integer.parseInt(v1.getText().toString()));
-                    soma+=vetor.get(cont);
                     cont++;
                     v1.setText("");
-                }else{
+                }
+                if(cont==10){
                     InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                    Toast.makeText(Ex10.this, "Média: "+soma/10, Toast.LENGTH_LONG).show();
+                    double soma=0;
+                    for(int i=0;i<10;i++){
+                        soma+=vetor.get(i);
+                    }
+                    Toast.makeText(Ex10.this, "Média: "+soma/10.0, Toast.LENGTH_LONG).show();
                     v1.setText("");
+                    cont=0;
+                    vetor.removeAll(vetor);
                 }
             }
         });
